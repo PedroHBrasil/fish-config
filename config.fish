6045fbbb5a280be5cf14fc802471add296df633d
Pedro H. Brasil <pedrohbrasil.pro@pm.me>
2025-05-09 11:35:59 -0300

    Set up to initialize on tmux session

--- config.fish
+++ config.fish
@@ -4,6 +4,15 @@ end
 
 set EDITOR nvim
 
+if not set -q TMUX
+    if tmux has-session -t default > /dev/null 2>&1
+        tmux attach -t default
+    else
+        tmux new-session -s default -A
+    end
+end
+
 starship init fish | source
 neofetch
 fortune
+
